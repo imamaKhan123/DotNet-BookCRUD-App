38d81d26f53b66f61329e3e1d328295156b705d4
användare <anvandare@MacBook-Pro-som-tillhor-anvandare.local>
2025-06-25 12:52:27 +0200

    added some changesto quotes

--- my-angular-app/src/environments/environment.ts
+++ my-angular-app/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
      apiUrl: 'https://dotnet-bookcrud-app-1.onrender.com'
-    //apiUrl: 'http://localhost:5050'
+   // apiUrl: 'http://localhost:5050'
  
   };
   
